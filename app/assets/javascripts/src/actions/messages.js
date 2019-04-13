b943df64924c8a1845b5f7535730bf14243c338b
Sayuriko <sayuri.gpro@gmail.com>
2019-04-13 13:02:58 -0400

    messages on react side working

--- app/assets/javascripts/src/actions/messages.js
+++ app/assets/javascripts/src/actions/messages.js
@@ -5,22 +5,23 @@ import Dispatcher from '../dispatcher'
 import {ActionTypes, APIEndpoints, CSRFToken} from '../constants/app'
 
 export default {
-  // changeOpenChat(newUserID) {
-    // Dispatcher.handleViewAction({
-      // type: ActionTypes.UPDATE_OPEN_CHAT_ID, // 変更箇所
-      // userID: newUserID,
-    // })
-  // },
+
+  changeOpenChat(newUserID) {
+    Dispatcher.handleViewAction({
+      type: ActionTypes.UPDATE_OPEN_CHAT_ID, // 変更箇所
+      userID: newUserID,
+    })
+  },
   // 追記
-  // sendMessage(userID, message) { // sendMessageの名前を変えるなら、replayBox.jsの
+  sendMessage(userID, message) { // sendMessageの名前を変えるなら、replayBox.jsの
     // MessagesAction.sendMessage->MessageAction.postMessageにかえる★
-    // Dispatcher.handleViewAction({
-      // type: ActionTypes.SEND_MESSAGE, // ここなどの定数関数をどうするか
-      // userID: userID,
-      // message: message,
-      // timestamp: +new Date(),
-    // })
-  // },
+    Dispatcher.handleViewAction({
+      type: ActionTypes.SEND_MESSAGE, // ここなどの定数関数をどうするか
+      userID: userID,
+      message: message,
+      timestamp: +new Date(),
+    })
+  },
 
 // getの場合
   getMessages() {
@@ -43,27 +44,27 @@ export default {
   }, // 続く場合,がいる
 
 // postの場合 // api/messages#create
-  sendMessage(content) {       // userと紐付けをした後に（）内をuserIDに変えていく？
-    return new Promise((resolve, reject) => {
-      request
-      .post(APIEndpoints.MESSAGES) // 後ほど説明します。
-      .set('X-CSRF-Token', CSRFToken()) // 後ほど説明します。
-      .send({content}) // ??これによりサーバ側に送りたいデータを送ることが出来ます。
-                       // ユーザーと紐づけた後に変更？
-      .end((error, res) => {
-        if (!error && res.status === 200) {
-          const json = JSON.parse(res.text)
-          Dispatcher.handleServerAction({
-            type: ActionTypes.SEND_MESSAGE,
-            // userId,  <-紐付けた後？
-            json,
-          })
-        } else {
-          reject(res)
-        }
-      })
-    })
-  },
+  // sendMessage(content) {       // userと紐付けをした後に（）内をuserIDに変えていく？
+  //   return new Promise((resolve, reject) => {
+  //     request
+  //     .post(APIEndpoints.MESSAGES) // 後ほど説明します。
+  //     .set('X-CSRF-Token', CSRFToken()) // 後ほど説明します。
+  //     .send({content}) // ??これによりサーバ側に送りたいデータを送ることが出来ます。
+  //                      // ユーザーと紐づけた後に変更？
+  //     .end((error, res) => {
+  //       if (!error && res.status === 200) {
+  //         const json = JSON.parse(res.text)
+  //         Dispatcher.handleServerAction({
+  //           type: ActionTypes.SEND_MESSAGE,
+  //           // userId,  <-紐付けた後？
+  //           json,
+  //         })
+  //       } else {
+  //         reject(res)
+  //       }
+  //     })
+  //   })
+  // },
 }
 
 // saveの場合
--- app/assets/javascripts/src/stores/messages.js
+++ app/assets/javascripts/src/stores/messages.js
@@ -5,89 +5,89 @@
 // そこから定数であるGET_MESSAGESなどを呼び出しているため
 import Dispatcher from '../dispatcher'  // ?
 import BaseStore from '../base/store'  // ?
-// import UserStore from '../stores/user' // ユーザー機能入れてから
+import UserStore from '../stores/user' // ユーザー機能入れてから
 import {ActionTypes} from '../constants/app'
 
 // ゲッター・セッター
 
-// const messages = {
-  // 2: {
-    // user: {
-      // profilePicture: 'https://avatars0.githubusercontent.com/u/7922109?v=3&s=460',
-      // id: 2,
-      // name: 'Ryan Clark',
-      // status: 'online',
-    // },
-    // lastAccess: {
-      // recipient: 1424469794050,
-      // currentUser: 1424469794080,
-    // },
-    // messages: [
-      // {
-        // contents: 'Hey!',
-        // from: 2,
-        // timestamp: 1424469793023,
-      // },
-      // {
-        // contents: 'Hey, what\'s up?',
-        // from: 1,
-        // timestamp: 1424469794000,
-      // },
-    // ],
-  // },
-  // 3: {
-    // user: {
-      // read: true,
-      // profilePicture: 'https://avatars3.githubusercontent.com/u/2955483?v=3&s=460',
-      // name: 'Jilles Soeters',
-      // id: 3,
-      // status: 'online',
-    // },
-    // lastAccess: {
-      // recipient: 1424352522000,
-      // currentUser: 1424352522080,
-    // },
-    // messages: [
-      // {
-        // contents: 'Want a game of ping pong?',
-        // from: 3,
-        // timestamp: 1424352522000,
-      // },
-    // ],
-  // },
-  // 4: {
-    // user: {
-      // name: 'Todd Motto',
-      // id: 4,
-      // profilePicture: 'https://avatars1.githubusercontent.com/u/1655968?v=3&s=460',
-      // status: 'online',
-    // },
-    // lastAccess: {
-      // recipient: 1424423579000,
-      // currentUser: 1424423574000,
-    // },
-    // messages: [
-      // {
-        // contents: 'Please follow me on twitter I\'ll pay you',
-        // timestamp: 1424423579000,
-        // from: 4,
-      // },
-    // ],
-  // },
-// }
+const messages = {
+  2: {
+    user: {
+      profilePicture: 'https://avatars0.githubusercontent.com/u/7922109?v=3&s=460',
+      id: 2,
+      name: 'Ryan Clark',
+      status: 'online',
+    },
+    lastAccess: {
+      recipient: 1424469794050,
+      currentUser: 1424469794080,
+    },
+    messages: [
+      {
+        contents: 'Hey!',
+        from: 2,
+        timestamp: 1424469793023,
+      },
+      {
+        contents: 'Hey, what\'s up?',
+        from: 1,
+        timestamp: 1424469794000,
+      },
+    ],
+  },
+  3: {
+    user: {
+      read: true,
+      profilePicture: 'https://avatars3.githubusercontent.com/u/2955483?v=3&s=460',
+      name: 'Jilles Soeters',
+      id: 3,
+      status: 'online',
+    },
+    lastAccess: {
+      recipient: 1424352522000,
+      currentUser: 1424352522080,
+    },
+    messages: [
+      {
+        contents: 'Want a game of ping pong?',
+        from: 3,
+        timestamp: 1424352522000,
+      },
+    ],
+  },
+  4: {
+    user: {
+      name: 'Todd Motto',
+      id: 4,
+      profilePicture: 'https://avatars1.githubusercontent.com/u/1655968?v=3&s=460',
+      status: 'online',
+    },
+    lastAccess: {
+      recipient: 1424423579000,
+      currentUser: 1424423574000,
+    },
+    messages: [
+      {
+        contents: 'Please follow me on twitter I\'ll pay you',
+        timestamp: 1424423579000,
+        from: 4,
+      },
+    ],
+  },
+}
 
-// var openChatID = parseInt(Object.keys(messages)[0], 10)// ?
+var openChatID = parseInt(Object.keys(messages)[0], 10)// ?
 
 class ChatStore extends BaseStore {
-  // getOpenChatUserID() {
-    // return openChatID  // getメソッドとsetメソッドに変える
-  // }
-  // getChatByUserID(id) {  //
-    // return messages[id]  //
-  // }
-  // getAllChats() { //
-    // return messages  //
-  // }
+  getOpenChatUserID() {
+    return openChatID  // getメソッドとsetメソッドに変える
+  }
+  getChatByUserID(id) {  //
+    return messages[id]  //
+  }
+  getAllChats() { //
+    return messages
+  }
   getChat() {
     if (!this.get('chatJson')) this.setChat([])
     return this.get('chatJson')
@@ -105,34 +105,35 @@ MessagesStore.dispatchToken = Dispatcher.register(payload => {
   // キーとしてユーザID、そして値にはユーザに関する様々なデータを入れています。
 
   switch (action.type) {
-    // case ActionTypes.UPDATE_OPEN_CHAT_ID:
-      // openChatID = payload.action.userID
-      // MessagesStore.emitChange()
-      // break
+    case ActionTypes.UPDATE_OPEN_CHAT_ID:
+      openChatID = action.userID
+      messages[openChatID].lastAccess.currentUser = +new Date()
+      MessagesStore.emitChange()
+      break
 
-    // case ActionTypes.SEND_MESSAGE:  // ??ユーザーモデル入れてからここを参考に下のcaseを変える?
-    // const userID = action.userID //
-      // messages[userID].messages.push({
-        // contents: action.message,
-        // timestamp: action.timestamp,
-        // from: UserStore.user.id // これを下のどこかにどうにかしていれる？
-      // })
-      // MessagesStore.emitChange()
-      // break
+    case ActionTypes.SEND_MESSAGE:  // ??ユーザーモデル入れてからここを参考に下のcaseを変える?
+      const userID = action.userID //
+        messages[userID].messages.push({
+          contents: action.message,
+          timestamp: action.timestamp,
+          from: UserStore.user.id // これを下のどこかにどうにかしていれる？
+        })
+        MessagesStore.emitChange()
+        break
 
     case ActionTypes.GET_MESSAGES: // 上のapi通信で使用したgetHogeアクションを受け取っているとします。
       MessagesStore.setChat(action.json) // getHogeで取得したjsonをセッターを利用して保存しています。
       MessagesStore.emitChange()
       break
 
-    case ActionTypes.SEND_MESSAGE:
-      const userID = action.userID  // たぶん
-      const messages = MessagesStore.getChat() // userに関すること
-      messages[userID].push( //
-        action.json.message
-        )
-      MessagesStore.emitChange()
-      break
+    // case ActionTypes.SEND_MESSAGE:
+    //   const userID = action.userID  // たぶん
+    //   const messages = MessagesStore.getChat() // userに関すること
+    //   messages[userID].push( //
+    //     action.json.message
+    //     )
+    //   MessagesStore.emitChange()
+    //   break
   }
   return true
 })
