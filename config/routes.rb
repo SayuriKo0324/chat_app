a42dd4d1b18c9b39ce6abe74ea30a48a30b5ab47
Sayuriko <sayuri.gpro@gmail.com>
2019-05-07 17:23:36 +0900

    fix-mypage

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,10 @@ Rails.application.routes.draw do
   resources :messages      # only[:create, :destroy] #
   root to: 'messages#index'
 
-  # devise_for :users
-
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions',
   :passwords => 'users/passwords'
  }
- resources :users, :only => [:show]
+  resources :users, :only => [:show]
 end
