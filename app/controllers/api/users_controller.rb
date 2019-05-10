d3b5b756c0ab4ea6bc4039061cb30ba47ccca150
Sayuriko <sayuri.gpro@gmail.com>
2019-05-10 10:17:20 +0900

    Type-Namestate

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,12 @@
 class API::UsersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @users = User.all
+    users = JSON.parse(request.body.read)
+    @users = User.where('name LIKE ?', "#{users["user_name"]}%")
     render json: @users
   end
 
   def show
     @user = User.find_by(id: params[:id])
   end
-end
+end 
